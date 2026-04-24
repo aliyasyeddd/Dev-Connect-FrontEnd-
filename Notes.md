1e48eef529eb6d95ba44cf331d565e158f0a9382
aliya mahaboob <122204472+aliyasyeddd@users.noreply.github.com>
2026-04-24 20:04:48 +0530

    adding redux tool for state management

---
+++ Notes.md
@@ -0,0 +1,7 @@
+-> cross origin request
+-> browser will block this request due to CORS policy, 
+-> you need to enable CORS on your backend server or use a proxy to make this request work 
+-> in development environment
+
+-> if your cookies are not set properly you it is not possible to make API call
+-> when you login a cookie is sent from server to you
---
+++ src/components/Login.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("aliyaasyedd@gmail.com");
+  const [password, setPassword] = useState("Aliyaaa45#");
+  const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+  //never call a hook inside a callback function, always call it at the top level of the component
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true },
+      );
+      //add user to redux store
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID:</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
